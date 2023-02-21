02a08264c54ac9f463709218fa9f5418f47af1d7
Andrew Conlin <andrew@andrewconl.in>
2023-02-21 22:00:38 +0000

    added commands framework + one for helmi to respond to

--- helmi.py
+++ helmi.py
@@ -1,19 +1,38 @@
 # author @andrwclnln
+# v1.0
 # feb 2023
 # a lil discord bot so that our hamster can join the fun
 
 import os
+import random
 
 import discord
+from discord.ext import commands
+
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.command(name='helmi')
+async def helmi(ctx):
+    helmiMessages = [
+    'squeak!',
+    'squeaksqueak',
+    'sqeuaksqueaksqueak',
+    '*chatter*',
+    '*bite*',
+    '*bites toe*',
+    '*angry noises*',
+    ]
+
+    response = random.choice(helmiMessages)
+    await ctx.send(response)
 
 client.run(TOKEN)
