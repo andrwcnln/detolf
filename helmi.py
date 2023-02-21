2b563f48e01750fbd7880a98c8e57340623668f9
Andrew Conlin <andrew@andrewconl.in>
2023-02-21 21:17:39 +0000

    initial commit

---
+++ helmi.py
@@ -0,0 +1,19 @@
+# author @andrwclnln
+# feb 2023
+# a lil discord bot so that our hamster can join the fun
+
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
